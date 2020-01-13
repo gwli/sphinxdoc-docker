81a883d4d8819f7cdf6c5fedbe8ab79e23d54d40
Victor Li <gwli@outlook.com>
2020-01-13 19:31:46 +0800

    add book_tml

---
+++ pandoc_ppt/cmd.sh
@@ -0,0 +1,4 @@
+#pandoc -t s5 -s habits.txt -o habits.html
+pandoc -t beamer -s habits.txt -o habits_beamer.pdf
+#https://pandoc.org/MANUAL.html#producing-slide-shows-with-pandoc
+#where FORMAT is either s5, slidy, slideous, dzslides, or revealjs
